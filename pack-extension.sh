4abe046b050fedc8066106a279b2fe3ac116df7e
gistart <gistart@yandex.ru>
2022-07-13 23:58:15 +0300

    refactoring & bugfixes; adds configuration UI; closes GH-5: no more deprecation warnings; closes GH-6 GH-9: precision of battery percentage;

--- pack-extension.sh
+++ pack-extension.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-zip -j tp_wattmeter.zip extension.js metadata.json
+# compile schema
+glib-compile-schemas schemas/
+
+# pack extension
+zip -r tp_wattmeter.zip \
+            extension.js \
+            prefs.js \
+            metadata.json \
+            schemas/gschemas.compiled \
+            schemas/org.gnome.shell.extensions.tp_wattmeter.gschema.xml
+
+# install extension
+gnome-extensions install ./tp_wattmeter.zip --force
+
+# test settings
+# gnome-extensions prefs tp_wattmeter@gistart
